2a1b768ecf156eade3f14c394b7be036b747f557
tomkallen <spbeat@mail.ru>
2016-11-12 12:15:05 +0300

    extend read method

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,45 @@
 class Lilly {
-    
+
     // Creating new instance of database with a name. Name becomes a prefix for database entries.
-    constructor(public name: string = ''){}
+    constructor(public name: string = '') {}
 
     // Attempt to establish a connection to local storage. If it is not available then return error.
     connect(callback: any) {
-        try{
-            localStorage.setItem("null","null");
+        try {
+            localStorage.setItem("null", "null");
             localStorage.removeItem("null");
-            return callback(null, true);
-        }
-        catch(e){
-            return callback(e);
+            return typeof callback == 'function' ? callback(null, true) : true;
+        } catch (e) {
+            return typeof callback == 'function' ? callback(e) : false;
         }
     }
 
-    save(key: string, data: any){
-        let value = JSON.stringify({"value": data});
-        try{
-            localStorage.setItem(this.name + key, value);
+    save(key: string, data: any) {
+        // Objectifying data to trap null and undefined data
+        data = JSON.stringify({
+            "_v": data
+        });
+        try {
+            localStorage.setItem(this.name + key, data);
+        } catch (e) {
+            console && console.log("Failed to save data. Storage is either full or out of reach.");
         }
-        catch(e){}
     }
-    read(){}
-    remove(){}
-    push(){}
-    pull(){}
-    destroy(){}
-    drop(){}
+    read(key: string, fallback: any) {
+        let data: any;
+        try{
+            data = JSON.parse(localStorage.getItem(this.name));
+        }
+        catch(e){
+            // If parse fails
+            localStorage[key]
+        }
+
+        return data === null? fallback: data._v !== 'undefined'? data._v: fallback;
+    }
+    remove() {}
+    push() {}
+    pull() {}
+    destroy() {}
+    drop() {}
 }
