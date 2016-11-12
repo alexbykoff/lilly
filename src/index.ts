7dae4b06870f2dfdcf58fc0c20c0607112ff7bba
tomkallen <spbeat@mail.ru>
2016-11-12 13:51:38 +0300

    add findKeys method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 class Lilly {
 
+    deny: Boolean = false;
     // Creating new instance of database with a name. Name becomes a prefix for database entries.
-    constructor(public name: string = '') {
-        this.name += "_";
+    constructor(public name: string = '', public access: string = '') {
+        if(this.name.length) this.name += '_';
+        if(this.access==='readonly') this.deny = true;
     }
 
     // Attempt to establish a connection to local storage. If it is not available then return error.
@@ -17,6 +19,7 @@ class Lilly {
     }
 
     save(key: string, data: any) {
+        if (this.deny) return;
         // Objectifying data to trap null and undefined data
         data = JSON.stringify({
             "_v": data
@@ -27,30 +30,54 @@ class Lilly {
             console && console.log("Failed to save data. Storage is either full or out of reach.");
         }
     }
-    read(key: string, fallback: any) {
+    find(key: string, fallback: any) {
         let data: any;
         try{
             data = JSON.parse(localStorage.getItem(this.name+key));
             console.log("Attempt to retreive", data);
         }
         catch(e){
-            // If parse fails
+            // If parse fails then parse it manually
             console.log("Parse fail. Objectifying the value");
             if(localStorage[key]){
                 data = {_v: localStorage.getItem(key)}
             }
+            // If no such data then fall back to default value
             else return fallback;
         }
 
         if (data === null) return fallback;
+        // Doublecheck data integrity
         if (typeof data ==='object' && data._v !== 'undefined'){
              console.log("Structure Ok. Data is", data._v);             
              return data._v;
         }
-        console.log("Not found, fallback")
+        console.log("Not found, fallback");
         return fallback;
     }
-    remove() {}
+
+    findKeys(){
+            let localKeys: any[] = [];
+            let prefix = this.name;
+        try{
+             Object.keys(localStorage)
+                .forEach(v=>{
+                    if (v.substr(0, prefix.length) === prefix) {
+                        localKeys.push(v);
+                    }
+                });
+        }
+        catch(e){
+            return;
+        }
+        
+        return localKeys;
+    }
+
+    remove(key: string) {
+        // This will silently terminate if no such data. No need to catch errors.
+        return localStorage.removeItem(this.name+key);
+    }
     push() {}
     pull() {}
     destroy() {}
