f3301e0d6db0f63ebe49729ab6d6dda863436386
tomkallen <spbeat@mail.ru>
2016-11-13 10:59:06 +0300

    add more comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 class Lilly {
 
-    deny: Boolean = false;
+    // setting writing permissions
+    readonly: boolean = false;
+
+    // container variable saves the state of the storage upon connection.
+    container: any[] = [];
+
     // Creating new instance of database with a name. Name becomes a prefix for database entries.
     constructor(public name: string = '', public access: string = '') {
         if (this.name.length) this.name += '_';
-        if (this.access === 'readonly') this.deny = true;
+        if (this.access === 'readonly') this.readonly = true;
     }
 
     // Attempt to establish a connection to local storage. If it is not available then return error.
+    // If connection is successfull then callback is executed
     connect(callback: any) {
+
         try {
             localStorage.setItem("null", "null");
             localStorage.removeItem("null");
@@ -18,8 +25,11 @@ class Lilly {
         }
     }
 
-    save(key: string, data: any) {
-        if (this.deny) return;
+    // Saves data into storage in JSON object format.
+    // Silently logs to console if error occurs.
+    save(key: string, data: any) : void {
+
+        if (this.readonly) return;
         // Objectifying data to trap null and undefined data
         data = JSON.stringify({
             "_v": data
@@ -30,14 +40,17 @@ class Lilly {
             console && console.log("Failed to save data. Storage is either full or out of reach.");
         }
     }
+    
+    // Looks for a key in a storage and checks its integrity.
+    // If values are not valid (for example - saved manually or by another script)
+    // Then method attempts to reconstuct data according to this library's format. 
     find(key: string, fallback: any) {
+
         let data: any;
         try {
-            data = JSON.parse(localStorage.getItem(this.name + key));
-            console.log("Attempt to retreive", data);
+            data = JSON.parse(localStorage.getItem(this.name + key));            
         } catch (e) {
-            // If parse fails then parse it manually
-            console.log("Parse fail. Objectifying the value");
+            // If parse fails then parse it manually        
             if (localStorage[key]) {
                 data = {
                     _v: localStorage.getItem(key)
@@ -49,14 +62,13 @@ class Lilly {
 
         if (data === null) return fallback;
         // Doublecheck data integrity
-        if (typeof data === 'object' && data._v !== 'undefined') {
-            console.log("Structure Ok. Data is", data._v);
+        if (typeof data === 'object' && data._v !== 'undefined') {            
             return data._v;
-        }
-        console.log("Not found, fallback");
+        }        
         return fallback;
     }
 
+    // Returns an array of keys, associated with this instance of storage
     findKeys() {
         let localKeys: any[] = [];
         let prefix = this.name;
@@ -80,16 +92,21 @@ class Lilly {
         return Object.keys(localStorage);
     }
 
-    remove(key: string) {
+    remove(key: string) : void {
+
         // This will silently terminate if no such data. No need to catch errors.
-        return localStorage.removeItem(this.name + key);
+        if (this.readonly) return;
+        localStorage.removeItem(this.name + key);
     }
     push() {}
     pull() {}
         
-    drop() {
+    drop() : void {
+
         // Destroys all local storage data associated with this particular database.
         // Keeps all other keys.
+
+        if (this.readonly) return;
         let keys = this.findKeys();
         keys.map(e => localStorage.removeItem(e));
     }
