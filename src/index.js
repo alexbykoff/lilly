c5e0222ff7e64a2bde2b4aaa7aca0524e1b074bb
Alex B <spbeat@mail.ru>
2018-04-10 16:40:01 +0300

    rewrite static to functions

--- src/index.js
+++ src/index.js
@@ -1,99 +1,96 @@
+const PREFIX = "li_";
+
 class LillyStore {
     store = {};
     readonly = false;
     collectionId = "";
     connected = false;
 
-    connect(collectionId = Lilly.error("no_collection"), callback) {
+    connect(collectionId = invokeError("no_collection"), callback) {
         if (this.connected) return;
-        Lilly.verifyLocalStorage();
-        this.collectionId = Lilly.prefix + collectionId;
-        const data = JSON.parse(localStorage.getItem(this.collectionId));
-        if (data) this.store = data;
+        verifyLocalStorage();
+        this.collectionId = PREFIX + collectionId;
+        const data = readFromStorage(collectionId);
         this.connected = true;
-        return { ...this.store };
+        if (data === null) return this.store;
+        this.store = { ...data };
+        return data;
     }
 
     save(id, data) {
-        if (!this.connected) Lilly.error("not_connected_yet");
+        if (!this.connected) invokeError("not_connected_yet");
         const updatedStore = { ...this.store, [id]: data };
-        Lilly.updateStorage(this.collectionId, updatedStore);
+        updateStorage(this.collectionId, updatedStore);
         this.store = { ...updatedStore };
         return data;
     }
 
     read(id) {
-        if (!this.connected) Lilly.error("not_connected_yet");
+        if (!this.connected) invokeError("not_connected_yet");
         if (!id in this.store) return null;
         return this.store[id];
     }
 
     drop() {
-        if (!this.connected) Lilly.error("not_connected_yet");
+        if (!this.connected) invokeError("not_connected_yet");
         localStorage.removeItem(this.collectionId);
         this.store = {};
         return true;
     }
 
     remove(id) {
-        if (!this.connected) Lilly.error("not_connected_yet");
+        if (!this.connected) invokeError("not_connected_yet");
         if (!id in this.store) return false;
         const updatedStore = { ...this.store };
         delete updatedStore[id];
-        Lilly.updateStorage(this.collectionId, updatedStore);
+        updateStorage(this.collectionId, updatedStore);
         this.store = { ...updatedStore };
         return updatedStore;
     }
 }
 
-class Lilly {
-    static prefix = "li_";
-
-    static error(error) {
-        let errorMessage = `Something is not right\n${error}`;
-        switch (error) {
-            case "no_collection":
-                errorMessage =
-                    "Collection name is not provided. This argument is needed to connect to collection or create a new one";
-                break;
-            case "no_connection":
-                errorMessage =
-                    "Could not connect to local storage. It is either full or not available";
-                break;
-            case "not_connected_yet":
-                errorMessage = "Collection is not connected";
-                break;
-        }
-        throw new Error(`Lilly error: ${errorMessage}`);
+function invokeError(code) {
+    let errorMessage = `Something is not right\n${error}`;
+    switch (error) {
+        case "no_collection":
+            errorMessage =
+                "Collection name is not provided. This argument is needed to connect to collection or create a new one";
+            break;
+        case "no_connection":
+            errorMessage =
+                "Could not connect to local storage. It is either full or not available";
+            break;
+        case "not_connected_yet":
+            errorMessage = "Collection is not connected";
+            break;
     }
+    throw new Error(`Lilly error: ${errorMessage}`);
+}
 
-    static verifyLocalStorage() {
-        try {
-            localStorage.setItem("null", "null");
-            localStorage.removeItem("null");
-        } catch (error) {
-            Lilly.error("no_connection");
-        }
-    }
-
-    static updateStorage(collectionId, data) {
-        try {
-            localStorage.setItem(collectionId, JSON.stringify(data));
-        } catch (error) {
-            Lilly.error(error);
-        }
+function updateStorage(collectionId, data) {
+    try {
+        localStorage.setItem(collectionId, JSON.stringify(data));
+    } catch (error) {
+        invokeError(error);
     }
 }
 
-const store = new LillyStore();
-store.connect("test04");
-store.save("name", "John");
-store.read("name");
+function verifyLocalStorage() {
+    try {
+        localStorage.setItem("null", "null");
+        localStorage.removeItem("null");
+    } catch (error) {
+        invokeError("no_connection");
+    }
+}
 
-const d = { 1: 2, 2: 3, 4: { f: 1, g: true } };
-
-store.save("myObj", d);
-
-const x = store.read("myObj");
-
-console.log(x);
+function readFromStorage(collectionId) {
+    let data;
+    try {
+        data = JSON.parse(localStorage.getItem(this.collectionId));
+    } catch (error) {
+        data = { data };
+    }
+    if (typeof data !== "object") data = { data };
+    return data;
+}
